fa6a36533507c3a3d4217b3165e66ec99634a235
Herman Bergwerf <post@hbergwerf.nl>
2025-10-16 19:40:16 +0200

    Add support for matching arrows.

--- api.py
+++ api.py
@@ -38,48 +38,75 @@ def read_vec2(data) -> tuple[float, float]:
         return (data['x'] / 100, data['y'] / 100)
 
 
-def json_to_graph(data) -> nx.Graph:
+def bond_edge(bond):
+    return bond['atoms'] if 'atoms' in bond else [bond['from'], bond['to']]
+
+
+def attach_arrow_endpoint(g: nx.Graph, endpoint, anchor, bonds):
+    if 'atom' in anchor:
+        g.add_edge(anchor['atom'], endpoint, type='arrow')
+    elif 'bond' in anchor:
+        i, j = bond_edge(bonds[anchor['bond']])
+        g.add_edge(i, endpoint, type='arrow')
+        g.add_edge(j, endpoint, type='arrow')
+        g.edges[(i,j)]['attached_to_arrow'] = True
+
+
+def json_to_graph(diagram, add_arrows=False) -> nx.Graph:
     g = nx.Graph()
 
-    for v, atom in enumerate(data['atoms']):
-        g.add_node(v, **{
+    for i, atom in enumerate(diagram['atoms']):
+        g.add_node(i, **{
+            'type': 'atom',
             'symbol': atom['symbol'],
             'position': read_vec2(atom['position']),
             'formal_charge': atom.get('formal_charge', 0),
             'non_bonded_ve': atom.get('non_bonded_ve', 0)
         })
 
-    for bond in data['bonds']:
-        if 'atoms' in bond:
-            u, v = bond['atoms']
-            g.add_edge(u, v, type=bond['type'])
-        else:
-            g.add_edge(bond['from'], bond['to'], type=bond['type'])
+    for bond in diagram['bonds']:
+        i, j = bond_edge(bond)
+        g.add_edge(i, j, type='bond', bond_type=bond['type'])
+
+    if 'arrows' in diagram and add_arrows:
+        for i, arrow in enumerate(diagram['arrows']):
+            start = f'arrow{i}start'
+            end = f'arrow{i}end'
+            attr = { 'type': 'arrow', 'arrow_type': arrow['type'] }
+            g.add_node(start, **attr, point='start')
+            g.add_node(end, **attr, point='end')
+            g.add_edge(start, end, type='arrow')
+            attach_arrow_endpoint(g, start, arrow['start'], diagram['bonds'])
+            attach_arrow_endpoint(g, end, arrow['end'], diagram['bonds'])
 
     return g
 
 
 def graph_to_mol(g: nx.Graph) -> Chem.rdchem.RWMol:
     mol = Chem.rdchem.RWMol()
-    index = {}
+    atoms = {}
 
-    for v in g.nodes:
-        a = Chem.rdchem.Atom(g.nodes[v]['symbol'])
-        a.SetFormalCharge(g.nodes[v]['formal_charge'])
-        index[v] = mol.AddAtom(a)
-
-    for u, v in g.edges:
-        type = g.edges[(u,v)]['type']
-        bond = mol.AddBond(index[u], index[v], bond_type[type])
-        if type in bond_dir:
-            mol.GetBondWithIdx(bond - 1).SetBondDir(bond_dir[type])
-
-    # Build 2D conformer.
+    # 2D conformer.
     conf = Chem.Conformer(mol.GetNumAtoms())
     conf.Set3D(False)
-    for v in g.nodes:
-        # Note that we add a Z-coordinate with value 0.
-        conf.SetAtomPosition(index[v], g.nodes[v]['position'] + (0,))
+
+    for i, attr in g.nodes(data=True):
+        if attr['type'] == 'atom':
+            a = Chem.rdchem.Atom(attr['symbol'])
+            a.SetFormalCharge(attr['formal_charge'])
+            atom_index = mol.AddAtom(a)
+            atoms[i] = atom_index
+
+            # Update conformer. Note that we add a Z-coordinate with value 0.
+            conf.SetAtomPosition(atom_index, attr['position'] + (0,))
+
+    for i, j, attr in g.edges(data=True):
+        if attr['type'] == 'bond':
+            type = attr['bond_type']
+            bond_count = mol.AddBond(atoms[i], atoms[j], bond_type[type])
+            bond = mol.GetBondWithIdx(bond_count - 1)
+            if type in bond_dir:
+                bond.SetBondDir(bond_dir[type])
 
     # Assign conformer and compute stereochemistry.
     mol = mol.GetMol()
@@ -104,60 +131,89 @@ def json_to_mol(data) -> Chem.rdchem.RWMol:
    return graph_to_mol(json_to_graph(data))
 
 
-def _node_match(v1, v2):
-    props = [
-        ['symbol', ''],
-        ['formal_charge', 0],
-        ['non_bonded_ve', 0],
-        ['mark', -1]
-    ]
-    return all(map(lambda p: v1.get(p[0], p[1]) == v2.get(p[0], p[1]), props))
+def match_props(x, y, props):
+    return all(map(lambda p: x.get(p[0], p[1]) == y.get(p[0], p[1]), props))
 
 
-def _graph_to_smiles(g: nx.Graph, isomeric = True) -> str:
+def match_nodes(v1, v2):
+    # We have atom nodes and arrow nodes.
+    if v1['type'] == 'atom':
+        return match_props(v1, v2, [
+            ['type', ''],
+            ['symbol', ''],
+            ['formal_charge', 0],
+            ['non_bonded_ve', 0],
+            ['mark', -1]
+        ])
+    elif v1['type'] == 'arrow':
+        return match_props(v1, v2, [
+            ['type', ''],
+            ['arrow_type', ''],
+            ['point', '']
+        ])
+    else:
+        return False
+
+
+def match_edges(e1, e2):
+    if e1['type'] != e2['type']:
+        return False
+    # We match bond types on bonds attached to arrows.
+    if e1['type'] == 'bond' and 'attached_to_arrow' in e1:
+        return e1['bond_type'] == e2['bond_type']
+    else:
+        return True
+
+
+def graph_to_smiles(g: nx.Graph, isomeric = True) -> str:
     mol = Chem.RemoveHs(graph_to_mol(g))
     return Chem.rdmolfiles.MolToSmiles(mol, isomericSmiles=isomeric)
 
 
-def _graph_hydrogens(g: nx.Graph) -> list:
-    # Remove hydrogens _when all other attributes are empty_.
-    return [n for n, attr in g.nodes(data=True) if attr['symbol'] == 'H'
-            and attr['formal_charge'] == 0 and attr['non_bonded_ve'] == 0]
+def graph_basic_hydrogens(g: nx.Graph) -> list:
+    # Select hdrogen atoms _where all other attributes are empty_.
+    return [v for v, attr in g.nodes(data=True) if
+            attr['type'] == 'atom' and
+            attr['symbol'] == 'H' and
+            attr['formal_charge'] == 0 and
+            attr['non_bonded_ve'] == 0]
 
 
 def compare_component(g1: nx.Graph, g2: nx.Graph, match_stereo: bool) -> bool:
     g1_no_h = g1.copy()
     g2_no_h = g2.copy()
-    g1_no_h.remove_nodes_from(_graph_hydrogens(g1_no_h))
-    g2_no_h.remove_nodes_from(_graph_hydrogens(g2_no_h))
+    g1_no_h.remove_nodes_from(graph_basic_hydrogens(g1_no_h))
+    g2_no_h.remove_nodes_from(graph_basic_hydrogens(g2_no_h))
+
     # Don't compare edge types to avoid filtering resonance structures.
-    if nx.is_isomorphic(g1_no_h, g2_no_h, _node_match):
+    if nx.is_isomorphic(g1_no_h, g2_no_h, match_nodes, match_edges):
         # This can fail because of non-standard valences, like in ClF3, which is
         # not supported by RDKit. Therefore, in the case of an error, we default
         # to accepting the bare isomorphism.
         try:
-            smi1 = _graph_to_smiles(g1, match_stereo)
-            smi2 = _graph_to_smiles(g2, match_stereo)
+            smi1 = graph_to_smiles(g1, match_stereo)
+            smi2 = graph_to_smiles(g2, match_stereo)
             return smi1 == smi2
         except ValueError:
             return True
 
 
 def compare(diagram1, diagram2, match_stereo = True) -> bool:
-    g1 = json_to_graph(diagram1)
-    g2 = json_to_graph(diagram2)
+    g1 = json_to_graph(diagram1, True)
+    g2 = json_to_graph(diagram2, True)
     cs1 = list(map(lambda c: g1.subgraph(c), nx.connected_components(g1)))
     cs2 = list(map(lambda c: g2.subgraph(c), nx.connected_components(g2)))
 
     # Check if every component in g1 matches a unique component in g2.
     for c1 in cs1:
-        match = (i for i, c2 in enumerate(cs2) if compare_component(c1, c2, match_stereo))
-        index = next(match, None)
-        if index is None:
+        matched = False
+        for i, c2 in enumerate(cs2):
+            if compare_component(c1, c2, match_stereo):
+                matched = True
+                cs2.pop(i)
+                break
+        if not matched:
             return False
-        else:
-            # Every component can only be matched once.
-            cs2.pop(index)
 
     return len(cs2) == 0
 
